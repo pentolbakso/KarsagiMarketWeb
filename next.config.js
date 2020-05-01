72b33f7403d0b8f15b386051b9b3087e10742495
Bayu Andrianto <bayufa@gmail.com>
2020-05-01 23:24:50 +0700

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
-const withPlugins = require('next-compose-plugins');
-const css = require('@zeit/next-css');
+const withPlugins = require("next-compose-plugins");
+const css = require("@zeit/next-css");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig = {
-  target: 'serverless',
+  target: "serverless",
   webpack(config) {
     config.module.rules.push({
       test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
       use: {
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 8192,
-          publicPath: '/_next/static/',
-          outputPath: 'static/',
-          name: '[name].[ext]',
+          publicPath: "/_next/static/",
+          outputPath: "static/",
+          name: "[name].[ext]",
         },
       },
     });
@@ -20,4 +23,6 @@ const nextConfig = {
   },
 };
 
-module.exports = withPlugins([[css]], nextConfig);
+const nextPlugins = [[css], [withBundleAnalyzer({})]];
+
+module.exports = withPlugins(nextPlugins, nextConfig);
