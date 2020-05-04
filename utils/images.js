9d6a2c3054fdd235cbe261f07c2b756eeb1d2aed
Bayu Andrianto <bayufa@gmail.com>
2020-05-05 01:45:19 +0700

    change image webp to jpeg

--- utils/images.js
+++ utils/images.js
@@ -5,8 +5,8 @@ export function imageOri(filename) {
   return `${STATIC_URL}/${UPLOAD_PATH}/ori/${filename}`;
 }
 export function image200(filename) {
-  return `${STATIC_URL}/${UPLOAD_PATH}/200/${filename}.webp`;
+  return `${STATIC_URL}/${UPLOAD_PATH}/200/${filename}.jpeg`;
 }
 export function image600(filename) {
-  return `${STATIC_URL}/${UPLOAD_PATH}/600/${filename}.webp`;
+  return `${STATIC_URL}/${UPLOAD_PATH}/600/${filename}.jpeg`;
 }
